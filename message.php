68c5dabe923c8a220ac23ab44adbf524e99e6e17
SAMIUL BASIR <samiulb25@gmail.com>
2024-07-17 02:40:49 +0600

    create a message which compare the number is greater than or less than.

---
+++ message.php
@@ -0,0 +1,16 @@
+<?php 
+
+    $x = 10; 
+    $y = 20;
+
+    if($x > $y){
+        $message = "x is greater than y.";
+    }
+    elseif($x < $y){
+        $message = "x is less than y.";
+    }
+    else{
+        $message = "x is equal to y.";
+    }
+    echo $message;
+?>
