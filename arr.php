97d790ce4a4dda918b3e32f6586f052e658d3697
SAMIUL BASIR <samiulb25@gmail.com>
2024-08-21 02:55:44 +0600

    Update array

--- arr.php
+++ arr.php
@@ -6,6 +6,7 @@
     $ass_array = ['name' => 'Samiul Basir', 'age' => 20];
     print_r($first_array);
     print_r($second_array);
-    print_r($ass_array);    
+    print_r($ass_array); 
+    print_r($ass_array['name'])
 
 ?>
