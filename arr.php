9f332af2b6f802d1756fca2ca2eac9724fb8c592
SAMIUL BASIR <samiulb25@gmail.com>
2024-08-21 02:27:15 +0600

    Creating an array and demonstrate also.

---
+++ arr.php
@@ -0,0 +1,11 @@
+<?php
+//Demonstrate array
+
+    $first_array = [20, 30,40];
+    $second_array = [50, 60,70 ];
+    $ass_array = ['name' => 'Samiul Basir', 'age' => 20];
+    print_r($first_array);
+    print_r($second_array);
+    print_r($ass_array);    
+
+?>
