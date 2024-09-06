5322c31f4801883dc390cf5346d33c7b7ac9edec
SAMIUL BASIR <samiulb25@gmail.com>
2024-09-07 04:53:24 +0600

    Demonstrate in array count function.

--- arr_func.php
+++ arr_func.php
@@ -3,6 +3,13 @@
     $my_arr = [0,1,2];
     // Using array unset functions
      unset($my_arr[2]);
-     print_r($my_arr)
+     print_r($my_arr);
+
+    //  Using array cont function
+    $c_arr = ['Masud', 'Chantu', 'Rubel'];
+    echo"<br>";
+    echo "My array count is: ";
+    echo count($c_arr);
+
 
 ?>
