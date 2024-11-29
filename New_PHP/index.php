7df68ca29336674cc32e82a0d0497a1a60717478
SAMIUL BASIR <samiulb25@gmail.com>
2024-11-29 06:46:40 +0600

    Just demonstrate basic array and loop with conditions.

--- New_PHP/index.php
+++ New_PHP/index.php
@@ -1,4 +1,18 @@
 <?php
 
 $title = 'I love doing php code!';
+if ('0'){
+    print('I love zero');
+}
+else{
+    print('I do not love zero');
+    $prices = [
+        'laptop' => 1000,
+        'mouse' => 50,
+        'keyboard' => 120
+    ];
+        print("<br />" . $prices['laptop'] . "<br />");
+        print($prices['mouse'] . "<br />");
+        print($prices['keyboard']);
+}
 require 'index.view.php';
