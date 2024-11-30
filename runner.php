715f9452bcd6bc895e15e5174bfe04f6b0e11536
SAMIUL BASIR <samiulb25@gmail.com>
2024-11-30 23:21:43 +0600

    Print the calculate area and shape name.

--- runner.php
+++ runner.php
@@ -2,6 +2,7 @@
     include 'circle.php';
    
     $circle = new Circle("Circle", 5);
-    var_dump($circle);
+    // var_dump($circle);
+    echo "Shape name is: " . $circle->getName() . "<br/>" . "The {$circle->getName()} area is: " . number_format($circle->calculateArea(), 2) ."<br/>";
 
 ?>
