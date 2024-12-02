c9f655a933b4e72577d0b89b9f759165875f6154
SAMIUL BASIR <samiulb25@gmail.com>
2024-12-03 02:40:09 +0600

    Connect with database and fetch with it.

--- New_PHP/pdo.php
+++ New_PHP/pdo.php
@@ -1,17 +1,15 @@
 <?php
-    $host = 'localhost';
-    $port = 3306;
-    $dbname ='mydata';
-    $username = 'root';
-    $password = 'admin#2024';
 
-    $dsn = "mysql:host={$host};port={$port};dbname={$dbname};charset=utf8";
+  include 'database.php';
 
-    try{
-        $pdo = new PDO($dsn, $username, $password);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo 'Connected successfully';
-    }catch(PDOException $e){
-        echo 'Connection failed: '. $e->getMessage();
-    }
+  $stmt = $pdo -> prepare('SELECT * FROM employees');
+
+  $stmt -> execute();
+
+  $results = $stmt -> fetchAll();
+
+  echo '</pre>';
+  var_dump($results);
+  echo '</pre>'; 
+
 ?>
