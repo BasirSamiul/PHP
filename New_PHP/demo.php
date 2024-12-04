9f1f389a0021d6d8a57f1bf714a572bd1e057f45
SAMIUL BASIR <samiulb25@gmail.com>
2024-12-04 09:57:01 +0600

    Define a constant name.

--- New_PHP/demo.php
+++ New_PHP/demo.php
@@ -5,5 +5,7 @@
     $results = call_user_func_array('add', [10,20]);
 
     echo $results; // Output: 30
+    define('APP_NAME', 'My Awesome App');
+    echo APP_NAME; // Output: My Awesome App
 
 ?>
